e02ca545abd6e6bdcc7e8e20c929d3aa516814dd
werner <werner_a_e@yahoo.es>
2018-04-14 21:23:45 -0300

    add specs for left join

--- spec/bones/sql_spec.cr
+++ spec/bones/sql_spec.cr
@@ -29,17 +29,23 @@ end
 class Position < Bones::TableDef
 end
 
+class Vehicle < Bones::TableDef
+end
+
 describe Bones::SQL do
   it "shows a verified typed query" do
     person = Person.new
     worker = Worker.new
     position = Position.new
+    vehicle = Vehicle.new
 
     person_id_column = IdColumn.new(person)
     worker_id_column = IdColumn.new(worker)
     position_id_column = IdColumn.new(position)
     worker_person_id_column = PersonIdColumn.new(worker)
     position_person_id_column = PersonIdColumn.new(position)
+    vehicle_person_id_column = PersonIdColumn.new(vehicle)
+
     person_age_column = AgeColumn.new(person)
     person_name_column = NameColumn.new(person)
     worker_name_column = NameColumn.new(worker)
@@ -51,6 +57,7 @@ describe Bones::SQL do
       .from(person)
       .inner_join(to_table: worker, on: person_id_column.eq(worker_person_id_column))
       .inner_join(to_table: position, on: person_id_column.dup.eq(position_person_id_column))
+      .left_join(to_table: vehicle, on: person_id_column.dup.eq(vehicle_person_id_column))
       .where(to_table: worker, column: worker_name_column.eq("Jhon"))
       .to_sql_string
       .should(
@@ -58,6 +65,7 @@ describe Bones::SQL do
         "FROM person " +
         "INNER JOIN worker ON person.id = worker.person_id " +
         "INNER JOIN position ON person.id = position.person_id " + 
+        "LEFT JOIN vehicle ON person.id = vehicle.person_id " + 
         "WHERE worker.name = 'Jhon'")
     )
   end
--- src/bones/sql.cr
+++ src/bones/sql.cr
@@ -2,13 +2,13 @@ module Bones
   class SQL
     property select_fields : Array(Select)
     property from_table : TableDef
-    property inner_join_tables : Array(Joins::InnerJoin)
+    property join_tables : Array(Joins::Join)
     property where : Array(Where)
 
     def initialize
       @select_fields = [] of Select
       @from_table = TableDef.new
-      @inner_join_tables = [] of Joins::InnerJoin
+      @join_tables = [] of Joins::Join
       @where = [] of Where
     end
 
@@ -23,7 +23,12 @@ module Bones
     end
 
     def inner_join(from_table = @from_table, to_table = TableDef.new, on = Column.new) : SQL
-      @inner_join_tables << Joins::InnerJoin.new(from_table, to_table, on)
+      @join_tables << Joins::InnerJoin.new(from_table, to_table, on)
+      self
+    end
+
+    def left_join(from_table = @from_table, to_table = TableDef.new, on = Column.new) : SQL
+      @join_tables << Joins::LeftJoin.new(from_table, to_table, on)
       self
     end
 
@@ -36,7 +41,7 @@ module Bones
       sql_string = ""
       sql_string = "SELECT #{@select_fields.map {|item| item.to_sql_string }.join(", ")}" unless @select_fields.empty?
       sql_string = "#{sql_string} FROM #{@from_table.to_sql_string}"
-      sql_string = "#{sql_string} #{@inner_join_tables.map {|join_table| join_table.to_sql_string }.join(" ")}" unless @inner_join_tables.empty?
+      sql_string = "#{sql_string} #{@join_tables.map {|join_table| join_table.to_sql_string }.join(" ")}" unless @join_tables.empty?
       sql_string = "#{sql_string} #{@where.map {|where| where.to_sql_string }.join(" ")}" unless @where.empty?
       sql_string
     end
